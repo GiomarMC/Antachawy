b8838ada7942a5818bf7edfa2e87cafbd2cc3562
Giomar <gmunozcu@unsa.edu.pe>
2024-07-01 23:48:14 -0500

    Actualizando archivo de paquetes

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name = "antachawy",
-    version = "0.1.0",
+    version = "0.0.1",
     packages = find_packages(where="include"),
     package_dir = {"": "include"},
 
     author = "Giomar Muñoz, Melany Cahuana, Andrea Cuela",
     author_email = "gmunozcu@unsa.edu.pe",
+    url = "https://github.com/GiomarMC/Antachawy.git",
     description = "Antachawy compiler package",
     license = "MIT",
     #entry_points = {
