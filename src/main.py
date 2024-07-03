d14fd515af24dfc530fcff69379c7209dc2cb1d5
Giomar <gmunozcu@unsa.edu.pe>
2024-07-03 02:12:08 -0500

    Se añade libreria consolehandler

--- src/main.py
+++ src/main.py
@@ -1,11 +1,9 @@
 from antachawy.scanner import Scanner
 from antachawy.token import Token
+from antachawy.consolehandler import ConsoleHandler
 
 import typer
 from pathlib import Path
-from rich.console import Console
-from rich.table import Table
-from rich import box
 
 def file_exists(path: str):
     file_path = Path(path)
@@ -16,17 +14,9 @@ def file_exists(path: str):
 def main(file_path: Path = typer.Argument("./inputs/main.awy", help="Archivo de entrada con codigo fuente", callback=file_exists)):
     scanner = Scanner()
     tokens = scanner.tokenize(str(file_path))
-
-    console = Console()
-    table = Table(title="Tokens Generados")
-    table.add_column("Lexema", style="cyan", justify="center")
-    table.add_column("Etiqueta", style="magenta", justify="center")
-    table.add_column("Linea", style="green", justify="center")
-
-    for token in tokens:
-        table.add_row(token.lexema, token.etiqueta, str(token.linea))
-    
-    console.print(table)
+    console_handler = ConsoleHandler()
+    console_handler.print_title()
+    console_handler.scan_debug_table(tokens)
 
 if __name__ == "__main__":
     typer.run(main)
