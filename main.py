fa8b929fa84c03d71e1397f379adf250524ce283
Giomar <gmunozcu@unsa.edu.pe>
2024-07-08 11:55:41 -0500

    Se retira main.py de carpeta src

---
+++ main.py
@@ -0,0 +1,23 @@
+from antachawy.scanner import Scanner
+from antachawy.token import Token
+from antachawy.consolehandler import ConsoleHandler
+
+import typer
+from pathlib import Path
+
+def file_exists(path: str):
+    file_path = Path(path)
+    if not file_path.exists():
+        raise typer.BadParameter(f"Archivo de entrada no encontrado: {path}")
+    return file_path
+
+def main(file_path: Path = typer.Argument("./inputs/main.awy", help="Archivo de entrada con codigo fuente", callback=file_exists)):
+    scanner = Scanner()
+    tokens = scanner.tokenize(str(file_path))
+    console_handler = ConsoleHandler()
+    console_handler.print_title()
+    console_handler.scan_debug_table(tokens)
+    console_handler.show_errors(scanner.errors)
+
+if __name__ == "__main__":
+    typer.run(main)
