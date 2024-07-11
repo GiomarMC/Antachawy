5e51905d91481107a0e9c2abc005644f34c5f0f3
Giomar <gmunozcu@unsa.edu.pe>
2024-07-11 17:25:54 -0500

    Se ajusta gramatica a operaciones basicas y se quita los puntos finales de cada produccion

--- antachawy/grammar.py
+++ antachawy/grammar.py
@@ -1,63 +1,31 @@
+epsilon = "ε"
 grammar = """
-<Programa> ::= <ListaDefiniciones> <Definicion>
-<Programa> ::=  <Definicion>
-<Definicion> ::= "qhapaq" "(" ")" "{" <Bloque> "}"
-<Definicion> ::= "qhapaq" "(" ")" "{""}"
-<Bloque> ::= <ListaCondiciones>
-<Bloque> ::= <Declaraciones>
-<Bloque> ::= "Siqiy" "(" <Expresion> ")"
-<Bloque> ::= <Operaciones>
-<ListaCondiciones> ::= <Condiciones> <ListaCondiciones>
-<ListaCondiciones> ::= <Condiciones>
-<Condiciones> ::= "ari" "(" <Condicion> ")" "{" <Bloque> "}" <CondicionesElseif> <CondicionElse>
-<Condiciones> ::= "ari" "(" <Condicion> ")" "{" <Bloque> "}" <CondicionElse>
-<Condiciones> ::= "ari" "(" <Condicion> ")" "{" <Bloque> "}"
-<Condiciones> ::= <cicloFor>
-<Condiciones> ::= <cicloWhile>
-<cicloFor> ::= "for" "(" <Declaracion> ";" <Condicion> ";" <Operacion> ")" "{" <Bloque> "}"
-<cicloWhile> ::= "while" "(" <Condicion> ")" "{" <Bloque> "}"
-<CondicionesElseif> ::= <CondicionElseif> <CondicionesElseif>
-<CondicionesElseif> ::= <CondicionElseif>
-<CondicionElseif> ::= "mana_chayqa_ari" "(" <Condicion> ")" "{" <Bloque> "}"
-<CondicionElse> ::= "mana_chayqa" "{" <Bloque> "}"
-<Declaraciones> ::= <Declaracion> <Declaraciones>
-<Declaraciones> ::= <Declaracion>
-<Declaracion> ::= <Tipo> <Asignacion>
-<Declaracion> ::= <Tipo> <Expresion>
-<Operaciones> ::= <Operacion> <Operaciones>
-<Operaciones> ::= <Operacion>
-<Operacion> ::= ID "=" ID <Operador> ID
-<Operacion> ::= ID "=" NUMERO <Operador> ID
-<Operacion> ::= ID "=" ID <Operador> NUMERO
-<Operacion> ::= ID "=" NUMERO <Operador> NUMERO
-<Asignacion> ::= ID "=" <Expresion>
-<Condicion> ::= <Expresion> <OperadorRelacional> <Expresion>
-<Condicion> ::= <Expresion>
-<Operador> ::= "+"
-<Operador> ::= "-"
-<Operador> ::= "*"
-<Operador> ::= "/"
-<OperadorRelacional> ::= ">"
-<OperadorRelacional> ::= "<"
-<OperadorRelacional> ::= "=="
-<OperadorRelacional> ::= "!="
-<OperadorRelacional> ::= "<="
-<OperadorRelacional> ::= ">="
-<Tipo> ::= "yupay"
-<Tipo> ::= "chunkayuq"
-<Tipo> ::= "sananpa"
-<Tipo> ::= "qaytu"
-<Tipo> ::= "bool"
-<Retorno> ::= "cutichiy" "(" <Expresion> ")"
-<Expresion> ::= <Expresion> <Operador> <Expresion>
-<Expresion> ::= ID <Expresion>
-<Expresion> ::= ID
-<Expresion> ::= NUMERO <Expresion>
-<Expresion> ::= NUMERO
-<Expresion> ::= CADENA <Expresion>
-<Expresion> ::= CADENA
-<Expresion> ::= "yanqa" <Expresion>
-<Expresion> ::= "yanqa"
-<Expresion> ::= "chiqaq" <Expresion>
-<Expresion> ::= "chiqaq"
+Programa -> Definicion
+Definicion -> qhapaq ( ) newline lbrace newline ListaSentencias rbrace
+ListaSentencias -> Sentencias ListaSentencias | ε
+Sentencias -> Declaraciones newline
+		| Asignaciones newline
+		| Impresiones newline
+Declaraciones -> Tipo id DeclaracionesPrime
+DeclaracionesPrime -> asignacion Expresion | ε
+Asignaciones -> id asignacion Expresion
+Impresiones -> siqiy ( ExpresionImpresion )
+Tipo -> yupay
+	| chunkayuq
+	| sananpa
+	| qaytu 
+    | bool
+Expresion -> Termino ExpresionPrime
+ExpresionPrime -> Operador Termino ExpresionPrime
+        | ε
+Operador -> + | menos | * | /
+Termino -> ( Expresion ) 
+	| id
+	| numero
+	| flotante
+	| caracter
+	| cadena
+ExpresionImpresion -> Expresion ExpresionImpresionPrime
+ExpresionImpresionPrime -> coma ExpresionImpresion
+			| ε
 """
