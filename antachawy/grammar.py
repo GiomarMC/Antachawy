dc4fb8e80980aa7d93c069cf30ef0aae4d12c483
Giomar <gmunozcu@unsa.edu.pe>
2024-07-16 02:51:59 -0500

    Se actualiza la gramatica teniendo en cuenta la jerarquia de operaciones

--- antachawy/grammar.py
+++ antachawy/grammar.py
@@ -2,32 +2,34 @@ epsilon = "ε"
 grammar = """
 Programa -> Definicion
 Definicion -> qhapaq ( ) newline lbrace newline ListaSentencias rbrace
-ListaSentencias -> Sentencias ListaSentencias | ε
+ListaSentencias -> Sentencias ListaSentenciasPrime
+ListaSentenciasPrime -> ListaSentencias | ε
 Sentencias -> Declaraciones newline
-		| Asignaciones newline
-		| Impresiones newline
+            | Asignaciones newline
+            | Impresiones newline
 Declaraciones -> Tipo id DeclaracionesPrime
 DeclaracionesPrime -> asignacion Expresion | ε
 Asignaciones -> id asignacion Expresion
 Impresiones -> siqiy ( ExpresionImpresion )
 Tipo -> yupay
-	| chunkayuq
-	| sananpa
-	| qaytu 
-    | bool
-Expresion -> Termino ExpresionPrime
-ExpresionPrime -> Operador Termino ExpresionPrime
-        | ε
-Operador -> + | menos | * | /
+      | chunkayuq
+      | sananpa
+      | qaytu 
+      | bool
+Expresion -> ExpresionMultiplicativa ExpresionAditivaPrime
+ExpresionAditivaPrime -> OperadorAditivo ExpresionMultiplicativa ExpresionAditivaPrime | ε
+ExpresionMultiplicativa -> Termino ExpresionMultiplicativaPrime
+ExpresionMultiplicativaPrime -> OperadorMultiplicativo Termino ExpresionMultiplicativaPrime | ε
+OperadorAditivo -> + | menos
+OperadorMultiplicativo -> * | /
 Termino -> ( Expresion ) 
-	| id
-	| numero
-	| flotante
-	| caracter
-	| cadena
-    | chinqaq
-    | yanqa
+         | id
+         | numero
+         | flotante
+         | caracter
+         | cadena
+         | chinqaq
+         | yanqa
 ExpresionImpresion -> Expresion ExpresionImpresionPrime
-ExpresionImpresionPrime -> coma ExpresionImpresion
-			| ε
+ExpresionImpresionPrime -> coma ExpresionImpresion | ε
 """
