e7fa65136fe0b3b83a1d5cd6b126f0bfe392630d
Giomar <gmunozcu@unsa.edu.pe>
2024-07-17 10:38:14 -0500

    Se añade el atributo para determinar el ambito de la variable

--- antachawy/symboltable.py
+++ antachawy/symboltable.py
@@ -2,14 +2,32 @@ class SymbolTable:
     def __init__(self):
         self.symbols = {}
 
-    def add(self, name, var_type):
+    def add(self, name, var_type, scope="global", value=None):
         if name in self.symbols:
             raise ValueError(f"Variable '{name}' ya definida.")
-        self.symbols[name] = var_type
+        self.symbols[name] = {"type": var_type, "scope": scope, "value": value}
 
     def get(self, name):
-        return self.symbols.get(name)
-
+        if name in self.symbols:
+            return self.symbols[name]["type"]
+        return None
+    
+    def get_value(self, name):
+        if name in self.symbols:
+            return self.symbols[name]["value"]
+        return None
+    
+    def set_value(self, name, value):
+        if name in self.symbols:
+            self.symbols[name]["value"] = value
+        else:
+            raise ValueError(f"Variable '{name}' no definida.")
+    
+    def get_scope(self, name):
+        if name in self.symbols:
+            return self.symbols[name]["scope"]
+        return None
+    
     def __repr__(self):
         return str(self.symbols)
     
@@ -19,7 +37,7 @@ class SymbolTable:
             return
         
         print("Tabla de Símbolos:")
-        print(f"{'Nombre':<15} {'Tipo':<10}")
-        print("="*25)
-        for name, var_type in self.symbols.items():
-            print(f"{name:<15} {var_type:<10}")
+        print(f"{'Nombre':<15} {'Tipo':<10} {'Ambito':<10} {'Valor':<10}")
+        print("="*45)
+        for name, info in self.symbols.items():
+            print(f"{name:<15} {info['type']:<10} {info['scope']:<10} {str(info['value']):<10}")
