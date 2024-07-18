84e06acb1137624690fbe8d9ff9da7a88983898c
Giomar <gmunozcu@unsa.edu.pe>
2024-07-18 02:30:35 -0500

    Se reorganiza el llamado de los analizadores y se coloca en funciones para una mayor modularidad y se añade el generador de codigo intermedio y optimizador

--- antachawy/compiler.py
+++ antachawy/compiler.py
@@ -3,6 +3,7 @@ from antachawy.parser import RecursiveDescentParser
 from antachawy.consolehandler import ConsoleHandler
 from antachawy.semantic import SemanticAnalyzer
 from antachawy.intermediate import IntermediateCodeGenerator
+from antachawy.optimizer import IntermediateCodeOptimizer
 
 class Compiler:
     def __init__(self, source_code: str):
@@ -12,6 +13,22 @@ class Compiler:
         self.console_handler = ConsoleHandler()
 
     def compile(self):
+        tokens = self.perform_lexical_analysis()
+        if not tokens:
+            return False
+
+        tree = self.perform_syntax_analysis(tokens)
+        if not tree:
+            return False
+
+        if not self.perform_semantic_analysis(tree):
+            return False
+
+        code = self.generate_intermediate_code(tree)
+        self.optimize_intermediate_code(code)
+        return True
+
+    def perform_lexical_analysis(self):
         tokens = self.scanner.tokenize(self.source_code)
         lex_errors = self.scanner.errors
 
@@ -19,31 +36,46 @@ class Compiler:
         if lex_errors:
             print("--> Lexical Analysis Failed")
             self.console_handler.show_errors(lex_errors)
-            return False
-        
-        print("--> Lexical Analysis Passed")
+            return None
 
+        print("--> Lexical Analysis Passed")
+        return tokens
+
+    def perform_syntax_analysis(self, tokens):
         self.parser = RecursiveDescentParser(self.scanner)
         tree = self.parser.parse()
         parse_errors = self.parser.errors
 
         if parse_errors:
             self.console_handler.show_errors(parse_errors)
-            return False
-        
-        print("--> Syntax Analysis Passed")
+            return None
 
+        print("--> Syntax Analysis Passed")
+        return tree
+
+    def perform_semantic_analysis(self, tree):
         analyzer = SemanticAnalyzer()
         analyzer.analyze(tree)
         if analyzer.errors:
             for error in analyzer.errors:
                 print(error)
             return False
-        
-        print("--> Semantic Analysis Passed")
-        generator = IntermediateCodeGenerator()
-        intermediate_code = generator.generate(tree)
-        for instruction in intermediate_code:
-            print(instruction)
 
+        print("--> Semantic Analysis Passed")
+        return True
+
+    def generate_intermediate_code(self, tree):
+        generator = IntermediateCodeGenerator()
+        code = generator.analyze(tree)
+        if code:
+            for cod in code:
+                print(cod)
+        print("--> Generated Intermediate Code")
+        return code
+    
+    def optimize_intermediate_code(self, code):
+        optimizer = IntermediateCodeOptimizer(code)
+        codigo = optimizer.optimize()
+        for cod in codigo:
+            print(cod)
         return True
