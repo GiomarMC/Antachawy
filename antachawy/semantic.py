8f43cd64c51b91002831e62de04f6a0eb6229c41
Giomar <gmunozcu@unsa.edu.pe>
2024-07-18 02:32:08 -0500

    Se modifica analizador semantico para que imprima en valor de cada variable

--- antachawy/semantic.py
+++ antachawy/semantic.py
@@ -1,13 +1,5 @@
 from antachawy.symboltable import SymbolTable
 
-class SemanticError:
-    def __init__(self, message, line):
-        self.message = message
-        self.line = line
-
-    def __repr__(self):
-        return f"Error en línea {self.line}: {self.message}"
-
 class SemanticAnalyzer:
     def __init__(self):
         self.symbol_table = SymbolTable()
@@ -25,6 +17,8 @@ class SemanticAnalyzer:
             self.visit_sentencias(node)
         elif node.name == "Declaraciones":
             self.visit_declaraciones(node)
+        elif node.name == "DeclaracionesPrime":
+            return self.visit_declaraciones_prime(node)
         elif node.name == "Asignaciones":
             self.visit_asignaciones(node)
         elif node.name == "Expresion":
@@ -39,7 +33,7 @@ class SemanticAnalyzer:
             return self.visit_termino(node)
         elif node.name == "Tipo":
             return self.visit_tipo(node)
-        return None
+        return None, None
 
     def visit_programa(self, node):
         for child in node.children:
@@ -60,72 +54,103 @@ class SemanticAnalyzer:
             self.visit(child)
 
     def visit_declaraciones(self, node):
-        tipo = self.visit(node.children[0])
+        tipo, _ = self.visit(node.children[0])
         nombre = node.children[1].children[0].name
+        _, valor = self.visit(node.children[2])
         try:
-            self.symbol_table.add(nombre, tipo, self.current_scope)
+            self.symbol_table.add(nombre, tipo, self.current_scope, valor)
         except Exception as e:
-            self.errors.append(e)
-        if len(node.children) > 2:
-            self.visit(node.children[2])
+            self.errors.append(str(e))
+
+    def visit_declaraciones_prime(self, node):
+        if len(node.children) > 1:
+            return self.visit(node.children[1])
+        return None, None
 
     def visit_asignaciones(self, node):
         nombre = node.children[0].children[0].name
-        expresion_tipo = self.visit(node.children[2])
+        expresion_tipo, valor = self.visit(node.children[2])
         tipo = self.symbol_table.get(nombre)
         if tipo is None:
-            self.errors.append(f"Error: Variable '{nombre}' no declarada.")
-        
+            self.errors.append(f"Error: Variable '{nombre}' no declarada.", node.line)
         if tipo != expresion_tipo:
             self.errors.append(f"Error de tipo: Variable '{nombre}' es de tipo '{tipo}' y la expresión es de tipo '{expresion_tipo}'.")
+        else:
+            self.symbol_table.set_value(nombre, valor)
 
     def visit_expresion(self, node):
-        return self.visit(node.children[0])
+        left_type, left_value = self.visit(node.children[0])
+        if node.children[1].children:
+            operator = node.children[1].children[0].children[0].name
+            right_type, right_value = self.visit(node.children[1])
+            if left_value and right_value:
+                if left_type != right_type:
+                    self.errors.append(f"Error de tipo: operación entre tipos '{left_type}' y '{right_type}' no permitida.")
+                valor = f"{left_value} {operator} {right_value}"
+                return left_type, valor
+        return left_type, left_value
 
     def visit_expresion_multiplicativa(self, node):
-        return self.visit(node.children[0])
+        left_type, left_value = self.visit(node.children[0])
+        if node.children[1].children:
+            operator = node.children[1].children[0].children[0].name
+            right_type, right_value = self.visit(node.children[1])
+            if left_value and right_value:
+                if left_type != right_type:
+                    self.errors.append(f"Error de tipo: operación entre tipos '{left_type}' y '{right_type}' no permitida.")
+                valor = f"{left_value} {operator} {right_value}"
+                return left_type, valor
+        return left_type, left_value
     
     def visit_expresion_aditiva_prime(self, node):
         if node.children:
-            left_type = self.visit(node.children[1])
-            right_type = self.visit(node.children[2])
-            if left_type != right_type:
-                self.errors.append(f"Error de tipo: operación entre tipos '{left_type}' y '{right_type}' no permitida.")
-            return left_type
+            left_type, left_value = self.visit(node.children[1])
+            operator = node.children[0].children[0].name
+            right_type, right_value = self.visit(node.children[2])
+            if left_value and right_value:
+                if left_type != right_type:
+                    self.errors.append(f"Error de tipo: operación entre tipos '{left_type}' y '{right_type}' no permitida.")
+                valor = f"{left_value} {operator} {right_value}"
+                return left_type, valor
+            return left_type, left_value
+        return None, None
+    
     def visit_expresion_multiplicativa_prime(self, node):
         if node.children:
-            left_type = self.visit(node.children[1])
-            right_type = self.visit(node.children[2])
-            if left_type != right_type:
-                self.errors.append(f"Error de tipo: operación entre tipos '{left_type}' y '{right_type}' no permitida.")
-            return left_type
+            left_type, left_value = self.visit(node.children[1])
+            operator = node.children[0].children[0].name
+            right_type, right_value = self.visit(node.children[2])
+            if left_value and right_value:
+                if left_type != right_type:
+                    self.errors.append(f"Error de tipo: operación entre tipos '{left_type}' y '{right_type}' no permitida.")
+                valor = f"{left_value} {operator} {right_value}"
+                return left_type, valor
+            return left_type, left_value
+        return None, None
         
     def visit_termino(self, node):
         child = node.children[0]
         if child.name == "ID":
             var_name = child.children[0].name
             var_type = self.symbol_table.get(var_name)
+            var_value = self.symbol_table.get_value(var_name)
             if var_type is None:
                 self.errors.append(f"Error: variable '{var_name}' no está definida.")
-            return var_type
+            return var_type, var_value
         elif child.name == "ENTERO":
-            return "yupay"
+            return "yupay", int(child.children[0].name)
         elif child.name == "FLOTANTE":
-            return "chunkayuq"
+            return "chunkayuq", float(child.children[0].name)
         elif child.name == "TRUE" or child.name == "FALSE":
-            return "bool"
+            return "bool", bool(child.children[0].name)
         elif child.name == "CARACTER":
-            return "sananpa"
+            return "sananpa", child.children[0].name
         elif child.name == "CADENA":
-            return "qaytu"
+            return "qaytu", child.children[0].name
     
     def visit_tipo(self, node):
-        return node.children[0].name
+        return node.children[0].name, None
 
     def analyze(self, root):
         self.visit(root)
-        if not self.errors:
-            print("--> Semantic Analysis Passed")
-        else:
-            print("--> Semantic Analysis Failed")
         self.symbol_table.print_table()
