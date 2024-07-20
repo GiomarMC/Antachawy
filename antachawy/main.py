a71a67dc9f840e20e88119a58344d4dd3047d117
Giomar <gmunozcu@unsa.edu.pe>
2024-07-19 20:25:27 -0500

    Se añade argumentos de argparse para la configuracion del CLI

--- antachawy/main.py
+++ antachawy/main.py
@@ -1,26 +1,50 @@
 import argparse
+import platform
 from antachawy.compiler import Compiler
 from pathlib import Path
 
-def file_exists(path: str):
-    file_path = Path(path)
-    if not file_path.exists():
-        raise argparse.ArgumentTypeError(f"Archivo de entrada no encontrado: {path}")
-    return file_path
+from antachawy import __version__
 
 def main():
-    parser = argparse.ArgumentParser(description="Antachawy compiler")
-    parser.add_argument("file_path", type=file_exists, help="Archivo de entrada con código fuente")
-    parser.add_argument("-o", "--output", type=str, help="Nombre del archivo de salida")
-    parser.add_argument("-d", "--debug", action="store_true", help="Genera archivos de depuración en la carpeta outputs")
+    parser = argparse.ArgumentParser(
+        description="Antachawy compiler CLI",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+        )
+    parser.add_argument(
+        "file_path",
+        type=Path,
+        help="Archivo de entrada con código fuente"
+        )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        help="Nombre del archivo de salida"
+        )
+    parser.add_argument(
+        "-d",
+        "--debug",
+        action="store_true",
+        help="Genera archivos de depuración en la carpeta outputs"
+        )
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="version",
+        version=f"Antachawy compiler version {__version__}",
+        help="Muestra la versión del compilador"
+        )
 
     args = parser.parse_args()
     
+    file_path = args.file_path
     output_file = args.output if args.output else None
     debug = args.debug
-    file_path = str(args.file_path)
     
-    compiler = Compiler(file_path, output_file, debug)
+    if not file_path.exists():
+        parser.error(f"El archivo {file_path} no existe")
+    
+    compiler = Compiler(str(file_path), output_file, debug)
     result = compiler.compile()
     
     if result:
